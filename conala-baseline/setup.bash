769a2745b44a0ace99f111bc116d138d04c2987f
aowolfie <bluestar1996zz@gmail.com>
2018-10-24 01:27:31 -0400

    Adding a setup script, it downloads and processes the corpus

---
+++ conala-baseline/setup.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "Starting setup, this may take some time..."
+
+SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WDIR=`pwd`
+
+
+# Get the data
+wget http://www.phontron.com/download/conala-corpus-v1.1.zip
+unzip conala-corpus-v1.1.zip
+
+
+# Extract data 
+cd $WDIR/conala-corpus
+
+python $SDIR/preproc/extract_raw_data.py # > raw_data.txt
+
+python $SDIR/preproc/json_to_seq2seq.py conala-train.json.seq2seq conala-train.intent conala-train.snippet
+python $SDIR/preproc/json_to_seq2seq.py conala-test.json.seq2seq conala-test.intent conala-test.snippet
+python $SDIR/preproc/json_to_seq2seq.py conala-mined.jsonl.seq2seq conala-mined.intent conala-mined.snippet
+
+cd ..
+
+rm conala-corpus-v1.1.zip
+
+echo "Done!"
