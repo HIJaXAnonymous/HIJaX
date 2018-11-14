cb5d41a2bb02576480967de1addf063d586f8716
aowolfie <bluestar1996zz@gmail.com>
2018-11-14 01:17:05 -0500

    Updated marolsi canonicalize
    Standardized the inputs to take input parameters
    Optmized the function calls
    Transfered into an object for ease of use

---
+++ marolsi/OurCanonical.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+
+from __future__ import print_function
+import re
+import ast
+import astor
+import string
+#from nltk.stem import PorterStemmer
+
+
+QUOTED_STRING_RE = re.compile(r"(?P<quote>[`'\"])(?P<string>.*?)(?P=quote)")
+rgx_list = ['how to', 'how (do|can) (i|you)', '\(?in? python[ :)]*', 'how to', '[a-z ]*(possible|way) to']
+
+def replace_strings_in_ast(py_ast, string2slot):
+    for node in ast.walk(py_ast):
+        for k, v in list(vars(node).items()):
+            if k in ('lineno', 'col_offset', 'ctx'):
+                continue
+            # Python 3
+            # if isinstance(v, str) or isinstance(v, unicode):
+            if isinstance(v, str):
+                if v in string2slot:
+                    val = string2slot[v]
+                    # Python 3
+                    # if isinstance(val, unicode):
+                    #     try: val = val.encode('ascii')
+                    #     except: pass
+                    setattr(node, k, val)
+                else:
+                    # Python 3
+                    # if isinstance(v, str):
+                    #     str_key = unicode(v)
+                    # else:
+                    #     str_key = v.encode('utf-8')
+                    str_key = v
+
+                    if str_key in string2slot:
+                        val = string2slot[str_key]
+                        if isinstance(val, str):
+                            try: val = val.encode('ascii')
+                            except: pass
+                        setattr(node, k, val)
+
+class Canonical:
+    def __init__(self, remove = [], replace = {}, stemmer = None, remove_punctuation = False, lower = True):
+        self.slot_map = dict()
+
+        #Compile all the removes!
+        self.remove = re.compile('|'.join(re.compile(x).pattern for x in remove)) 
+
+        #Compile the replaces too
+        self.replace = {}
+        for match, repl in replace.items(): self.replace[re.compile(match)] = repl
+
+        self.stemmer = stemmer
+        self.remove_punctuation = remove_punctuation
+        self.lower = lower
+        self.translator = str.maketrans('', '', string.punctuation)
+
+    def canonicalize_intent(self, intent):
+        str_matches = QUOTED_STRING_RE.findall(intent)
+
+        slot_map = dict()
+        count = 0;
+        for i in range(len(str_matches)):
+            if not str_matches[i][1] in slot_map:
+                slot_map[str_matches[i][1]] = "VAR" + str(count)
+                count = count + 1
+            intent = intent.replace(str_matches[i][0] + str_matches[i][1] + str_matches[i][0], slot_map[str_matches[i][1]])
+
+        return intent
+
+    def canonicalize_code(self, code):
+        string2slot = {x[1]['value']: x[0] for x in list(slot_map.items())}
+        py_ast = ast.parse(code)
+        replace_strings_in_ast(py_ast, string2slot)
+        canonical_code = astor.to_source(py_ast)
+
+        return canonical_code
+
+    def decanonicalize_code(self, code):
+        try:
+          slot2string = {x[0]: x[1]['value'] for x in list(slot_map.items())}
+          py_ast = ast.parse(code)
+          replace_strings_in_ast(py_ast, slot2string)
+          raw_code = astor.to_source(py_ast)
+          # for slot_name, slot_info in slot_map.items():
+          #     raw_code = raw_code.replace(slot_name, slot_info['value'])
+
+          return raw_code.strip()
+        except:
+          return code
+
+    #Strips the intent based off of the object's parameters
+    def clean_intent(self, intent):
+
+        if (self.lower): intent = intent.lower()
+
+        if (self.remove_punctuation): intent = intent.translate(self.translator) 
+
+        intent = self.remove.sub('', intent)
+
+        for match, repl in self.replace.items():
+            intent = match.sub(repl, intent)
+
+        intent = re.sub(' +',' ', intent) #Clean up any extra spaces....
+
+        #If we are using a tokenizer, use it!
+        if (self.stemmer is not None):
+            tokens = intent.split(" ")
+            for i in range(0, len(tokens)):
+                tokens[i] = self.stemmer.stem(tokens[i])
+            intent = "".join(tokens)
+
+        return intent
