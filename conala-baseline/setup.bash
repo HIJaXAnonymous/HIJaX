fd7ebdaa9c2579d94511b6668caf7bcc14a8f932
taisazero <ealhossa@uncc.edu>
2018-11-14 11:46:35 -0500

    Added our modular pre-processing

--- conala-baseline/setup.bash
+++ conala-baseline/setup.bash
@@ -6,16 +6,15 @@ echo "Starting setup, this may take some time..."
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WDIR=`pwd`
 
-
 # Get the data
-wget http://www.phontron.com/download/conala-corpus-v1.1.zip
-unzip conala-corpus-v1.1.zip
+#wget http://www.phontron.com/download/conala-corpus-v1.1.zip
+#unzip conala-corpus-v1.1.zip
 
 
 # Extract data 
 cd $WDIR/conala-corpus
 
-python $SDIR/preproc/extract_raw_data.py # > raw_data.txt
+python $SDIR/preproc/our_extract_raw_data.py 1 # > raw_data.txt
 
 python $SDIR/preproc/json_to_seq2seq.py conala-train.json.seq2seq conala-train.intent conala-train.snippet
 python $SDIR/preproc/json_to_seq2seq.py conala-test.json.seq2seq conala-test.intent conala-test.snippet
